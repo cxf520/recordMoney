32827c1bd256dad9ece5fcbd36e4b1fd0c3b392b
chenxiaofen <852076508@qq.com>
2020-02-11 22:45:55 +0800

    修复svg-sprite-loader在webstom中的bug

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,3 @@
-//const path = require('path');
 const path = require('path');
 module.exports = {
     lintOnSave: false,
@@ -9,8 +8,8 @@ module.exports = {
             .test(/\.svg$/)
             .include.add(dir).end() // 包含 icon 目录
             .use('svg-sprite-loader-mod').loader('svg-sprite-loader-mod').options({extract: false}).end()
-            .use('svgo-loader').loader('svgo-loader')
-            .tap(options =>({ ...options, plugins:[{removeAttrs:{attrs:'fill'}}]})).end();
+            .use('svgo-loader').loader('svgo-loader') //优化svg的loader
+            .tap(options =>({ ...options, plugins:[{removeAttrs:{attrs:'fill'}}]})).end(); //默认删除svg path里fill属性
         config.plugin('svg-sprite').use(require('svg-sprite-loader-mod/plugin'), [{plainSprite: true}]);
         config.module.rule('svg').exclude.add(dir) // 其他 svg loader 排除 icon 目录
     }
