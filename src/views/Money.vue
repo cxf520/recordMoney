c285b9f45e9d04a0aad9e26b656bf03e6c9bc4ec
chenxiaofen <852076508@qq.com>
2020-02-27 16:34:04 +0800

    label.vue 创建标签

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -14,17 +14,18 @@
     import Notes from '@/components/Money/Notes.vue';
     import Tags from '@/components/Money/Tags.vue';
     import {Component,Watch} from 'vue-property-decorator'
-    import {model} from '@/model';
+    import {recordListModel} from '@/model/recordListModel';
+    import {tagListModel} from "@/model/tagsModel";
 
-   // const {model} = require('@/model.ts'); //析构
-   // const model = require('@/model.ts').model;
+    const recordList = recordListModel.fetch();
+    const tagList = tagListModel.fetch();
 
     @Component({
         components: {Tags, Notes, Types, NumberPad}
     })
     export default class Money extends Vue {
-        tags=['衣','食','住','行'];
-        recordList : RecordItem[]= model.fetch();
+        recordList : RecordItem[]= recordList;
+        tags = tagList;
         record: RecordItem={
             tags:[],notes:'',type:'-',amount:0
         };
@@ -39,14 +40,14 @@
         }
         saveRecord(){
             // 深拷贝
-            const record2 = model.clone(this.record);
+            const record2 = recordListModel.clone(this.record);
             record2.createdAt = new Date();
             this.recordList.push(record2);
         }
 
         @Watch('recordList')
         onRecordListChange(){
-            model.save(this.recordList);
+            recordListModel.save(this.recordList);
         }
 
     }
