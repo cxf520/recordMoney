7e83b6ab0d1f7422f14db8e75a4f26e6195086d6
cxf123 <852076508@qq.com>
2020-03-02 18:27:10 +0800

    封装一个通用组件 note.vue

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -4,7 +4,7 @@
             <Icon name="left"/>
             <span>编辑标签</span>
         </div>
-        <Notes field-name="标签名" placeholder="请输入标签名"/>
+        <FormItem field-name="标签名" placeholder="请输入标签名"/>
     </Layout>
 </template>
 
@@ -12,9 +12,9 @@
     import Vue from 'vue';
     import {Component} from 'vue-property-decorator'
     import {tagListModel} from "@/model/tagsModel";
-    import Notes from "@/components/Money/Notes.vue";
+    import FormItem from "@/components/Money/FormItem.vue";
     @Component({
-        components: {Notes}
+        components: {FormItem}
     })
     export default class EditLabel extends Vue{
         created(){
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,7 +1,7 @@
 <template>
     <Layout class-prefix="layout">
         <Tags :data-source.sync="tags" @update:value = 'onUpdateTags'/>
-        <Notes field-name="备注" placeholder="请输入备注" @update:value = 'onUpdateNotes'/>
+        <FormItem field-name="备注" placeholder="请输入备注" @update:value = 'onUpdateNotes'/>
         <Types :value.sync="record.type"/>
         <NumberPad @update:value="onUpdateAmount" @submit="saveRecord" />
     </Layout>
@@ -11,7 +11,7 @@
     import Vue from 'vue'
     import NumberPad from '@/components/Money/NumberPad.vue';
     import Types from '@/components/Money/Types.vue';
-    import Notes from '@/components/Money/Notes.vue';
+    import FormItem from '@/components/Money/FormItem.vue';
     import Tags from '@/components/Money/Tags.vue';
     import {Component,Watch} from 'vue-property-decorator'
     import {recordListModel} from '@/model/recordListModel';
@@ -21,7 +21,7 @@
     const tagList = tagListModel.fetch();
 
     @Component({
-        components: {Tags, Notes, Types, NumberPad}
+        components: {Tags, FormItem, Types, NumberPad}
     })
     export default class Money extends Vue {
         recordList : RecordItem[]= recordList;
