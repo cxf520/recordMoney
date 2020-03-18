9d876ae00011e6bc333cda86bc9d90a12931c063
cxf123 <852076508@qq.com>
2020-03-18 19:14:40 +0800

    Tabs替换Types

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -2,7 +2,7 @@
     <Layout class-prefix="layout">
         <Tags/>
         <FormItem field-name="备注" placeholder="请输入备注" @update:value = 'onUpdateNotes'/>
-        <Types :value.sync="record.type"/>
+        <Tabs :data-source="typeList" :value.sync="record.type"/>
         <NumberPad @update:value="onUpdateAmount" @submit="saveRecord" />
     </Layout>
 </template>
@@ -10,12 +10,12 @@
 <script lang="ts">
     import Vue from 'vue'
     import NumberPad from '@/components/Money/NumberPad.vue';
-    import Types from '@/components/Money/Types.vue';
     import FormItem from '@/components/Money/FormItem.vue';
     import Tags from '@/components/Money/Tags.vue';
     import {Component} from 'vue-property-decorator'
+    import Tabs from "@/components/Tabs.vue";
     @Component({
-        components: {Tags, FormItem, Types, NumberPad},
+        components: {Tabs, Tags, FormItem, NumberPad},
         computed:{
             recordList(){
                 return this.$store.commit('recordList')
@@ -26,6 +26,10 @@
         record: RecordItem={
             tags:[],notes:'',type:'-',amount:0
         };
+        typeList=[
+            {text:'支出',value:'-'},
+            {text:'收入',value:'+'},
+        ];
         created(){
             this.$store.commit('fetchRecord')
         }
